345d1233803901f3957cbc47f2f8a68666c37b69
Gustave MEHADDI <mehaddi@d17-l428-pc-04.ipa.iisg.agh.edu.pl>
2018-12-04 10:23:11 +0100

    initial vue.js project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>  
+    <h1>{{ title }}</h1>
+    <button @click="iAmClicked()">Click me!</button>
+  </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {title: 'Not clicked yet'}
+        },
+        methods: {
+            iAmClicked() {
+                this.title = 'Clicked %d% times';
+            }
+        }
+    };
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
